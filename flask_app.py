13ff96eb8bf70dcb79cbd13349ca2a04283b39ee
Michael Plews <michael.plews@gmail.com>
2024-01-09 21:00:42 -0800

    first commit

--- flask_app.py
+++ flask_app.py
@@ -6,16 +6,16 @@ import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 
-def fahrenheit_from(celsius):
-    """Convert Celsius to Fahrenheit degrees."""
-    try:
-        fahrenheit = float(celsius) * 9 / 5 + 32
-        fahrenheit = round(fahrenheit, 3)  # Round to three decimal places
-        return str(fahrenheit)
-    except ValueError:
-        return "invalid input"
+# def fahrenheit_from(celsius):
+#     """Convert Celsius to Fahrenheit degrees."""
+#     try:
+#         fahrenheit = float(celsius) * 9 / 5 + 32
+#         fahrenheit = round(fahrenheit, 3)  # Round to three decimal places
+#         return str(fahrenheit)
+#     except ValueError:
+#         return "invalid input"
 
 @app.route('/')
 def hello_world():
-    celsius = input('Celsius: ')
-    return 'Hello from Flask!' + fahrenheit_from(celsius)
+    # celsius = input('Celsius: ')
+    return 'Hello from Flask!' #+ fahrenheit_from(celsius)
